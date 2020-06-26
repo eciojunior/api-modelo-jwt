42727a9b2b3d19c3fd885cd7d15b6f2a23da5abd
Ecio Junior Ferreira <ecio.ferreira@sankhya.com.br>
2020-06-26 15:54:51 -0300

    opa

--- src/main/java/br/com/model/controller/ModelController.java
+++ src/main/java/br/com/model/controller/ModelController.java
@@ -2,6 +2,8 @@ package br.com.model.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +29,8 @@ public class ModelController {
 	@GetMapping("/hello")
 	@PreAuthorize("hasAuthority('ADMIN')")
 	@ResponseBody
-	public String hello() {
+	public String hello(@AuthenticationPrincipal Jwt jwt) {
+		System.out.println(jwt.getClaimAsString("name")); // Caso precisar acessar os dados do JWT
 		return modelService.hello();
 	}
 }
