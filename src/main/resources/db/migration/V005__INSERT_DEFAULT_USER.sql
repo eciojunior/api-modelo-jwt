dbd351a3d7bae3bd68e293fd4ad58f03eb01a0d1
Ecio Junior Ferreira <ecio.ferreira@sankhya.com.br>
2020-06-30 12:26:14 -0300

    validacoes

---
+++ src/main/resources/db/migration/V005__INSERT_DEFAULT_USER.sql
@@ -0,0 +1,5 @@
+INSERT INTO public."user"
+("name", email, cel, "password", wallet, available, authority, photo, indicate)
+VALUES('André Brant', 'andrerbrant@gmail.com', NULL, '$2a$10$T48igJaNL7KoEuSwdvVku.R60/cMpZSqvy624sceg0rTeH6AsXHTa', NULL, true, 'ADMIN', NULL, NULL);
+
+
